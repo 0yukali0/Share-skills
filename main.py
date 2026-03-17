b61f24bdd3a2c2f8a709b9b81ede621f9e3066d7
yuteng <a08h0283@gmail.com>
2026-03-17 11:04:31 +0800

    agent type
    Signed-off-by: yuteng <a08h0283@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os
+from typing import Dict, List
 
 import gradio as gr
 import requests
 from openai import OpenAI
 from openrouter import OpenRouter
+
+messages: List[Dict[str, str]] = [
+    {
+        "role": "system",
+        "content": """
+    Your goals are fulfill users' tasks, you must choose one of follow to respond:
+    1. If executing the command can fulfill users' tasks then output format will be 'Execute command: xxx'. xxx is the command which you do not to describes.
+    2. If executing the commmand is not required, then output 'Finished: xxx', xxx is the summary.
+    """,
+    }
+]
+
 
 def call_model(message: str, model: str, provider: str = "ollama") -> str:
     if provider == "ollama":
@@ -13,10 +26,8 @@ def call_model(message: str, model: str, provider: str = "ollama") -> str:
     else:
         return f"Unknown provider: {provider}"
 
+
 def ensure_model_exists(model: str):
-    """
-    確保模型存在（沒有就 pull）
-    """
     tags = requests.get("http://localhost:11434/api/tags").json()
 
     models = [m["name"] for m in tags.get("models", [])]
@@ -29,15 +40,12 @@ def ensure_model_exists(model: str):
 
 
 def unload_model(model: str):
-    """
-    卸載模型（釋放記憶體）
-    """
     requests.post(
         "http://localhost:11434/api/generate",
         json={
             "model": model,
             "prompt": "",
-            "keep_alive": 0,  # 🔥 關鍵：立即 unload
+            "keep_alive": 0,
         },
     )
 
@@ -45,17 +53,29 @@ def unload_model(model: str):
 def greet_ollama(message: str, model: str = "llama3") -> str:
     ensure_model_exists(model)
     # check local allama server start a llm instance based on model parameter
-    client = OpenAI(
-        base_url="http://localhost:11434/v1",
-        api_key="ollama",
-    )
-    response = client.chat.completions.create(
-        model=model,  # 換成你本地模型
-        messages=[{"role": "user", "content": message}],
-    )
+    while True:
+        client = OpenAI(
+            base_url="http://localhost:11434/v1",
+            api_key="ollama",
+        )
+        messages.append({"role": "user", "content": message})
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages,
+        )
+        res = response.choices[0].message.content
+        messages.append({"role": "assistant", "content": res})
+        print(f"[AI] {res}")
+        if res.strip().startswith("Finished:"):
+            print("[AI] Task finished.")
+            break
+        command = res.strip().split("Execute command:")[1].strip()
+        command_result = os.popen(command).read()
+        print(f"[Command Result] {command_result}")
+        messages.append({"role": "user", "content": command_result})
     # shut down local ollama llm instance
     unload_model(model)
-    return response.choices[0].message.content
+    return res
 
 
 def greet_world(message: str, model: str = "minimax/minimax-m2") -> str:
@@ -71,7 +91,10 @@ def greet_world(message: str, model: str = "minimax/minimax-m2") -> str:
 
 if __name__ == "__main__":
     demo = gr.Interface(
-        fn=call_model, inputs=["text", "text", "text"], outputs=["text"], api_name="predict"
+        fn=greet_ollama,
+        inputs=["text"],
+        outputs=["text"],
+        api_name="predict",
     )
 
     demo.launch()
