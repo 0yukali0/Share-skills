1dd016542739fc4fc97b231bd28e72eb6dd80639
yuteng <a08h0283@gmail.com>
2026-03-17 14:09:26 +0800

    read agent md
    Signed-off-by: yuteng <a08h0283@gmail.com>

---
+++ agent.md
@@ -0,0 +1,4 @@
+Your goals are fulfill users' tasks, you must choose one of follow to respond:
+
+1. If executing the command can fulfill users' tasks then output format will be 'Execute command: xxx'. xxx is the command which you do not to describes.
+2. If executing the commmand is not required, then output 'Finished: xxx', xxx is the summary.
--- main.py
+++ main.py
@@ -9,14 +9,9 @@ from openrouter import OpenRouter
 messages: List[Dict[str, str]] = [
     {
         "role": "system",
-        "content": """
-    Your goals are fulfill users' tasks, you must choose one of follow to respond:
-    1. If executing the command can fulfill users' tasks then output format will be 'Execute command: xxx'. xxx is the command which you do not to describes.
-    2. If executing the commmand is not required, then output 'Finished: xxx', xxx is the summary.
-    """,
+        "content": open("agent.md").read(),
     }
 ]
-
 
 def call_model(message: str, model: str, provider: str = "ollama") -> str:
     if provider == "ollama":
